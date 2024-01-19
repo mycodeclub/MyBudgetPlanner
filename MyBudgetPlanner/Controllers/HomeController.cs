a175bf286e2688b7c0c145c22bc44ace21a73c3a
Sanjay Kumar <ankitsahay1990@gmail.com>
2024-01-19 11:06:19 +0530

    added dynamic graph on Dashboard

--- MyBudgetPlanner/Controllers/HomeController.cs
+++ MyBudgetPlanner/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using MyBudgetPlanner.DataBase;
 using MyBudgetPlanner.Models;
 using System.Diagnostics;
+using System.Linq;
 
 namespace MyBudgetPlanner.Controllers
 {
@@ -54,5 +56,73 @@ namespace MyBudgetPlanner.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public async Task<ActionResult> GetChartData()
+        {
+            var expensePlan = await _context.MyExpensePlans.Where(p => p.UserId.Equals(GetLoggedInUserId())).ToListAsync();
+            var DailyExpense = await _context.MyDailyExpenses.Where(p => p.UserId.Equals(GetLoggedInUserId())).ToListAsync();
+            var income = await _context.Incomes.Where(predicate: p => p.UserId.Equals(GetLoggedInUserId()) && p.Year.Equals(DateTime.Now.Year) && p.Month.Equals(DateTime.Now.Month)).ToListAsync();
+            var expenseAmount = expensePlan.Select(d => d.Amount).ToList();
+            var expenseName = expensePlan.Select(d => d.ExpenseName).ToList();
+            var UniqueItemId = expensePlan.Select(d => d.UniqueId).ToList();
+            var sumByUniqueId = _context.MyDailyExpenses
+             .Where(item => UniqueItemId.Contains((Guid)item.ExpenseUnderPlanId))
+             .GroupBy(item => item.ExpenseUnderPlanId)
+             .Select(group => new
+             {
+                 ExpenseUnderPlanId = group.Key,
+                 SumAmount = group.Sum(item => item.Amount)
+             })
+             .ToList();
+            var expenseColors = GenerateRandomColors(expenseName.Count);
+            var planData = expenseAmount.ToArray();
+            var expenseData = sumByUniqueId.Select(d => d.SumAmount).ToArray();
+
+            var chartData = new
+            {
+                labels = expenseName.ToArray(),
+                datasets = new[]
+                {
+                new
+                {
+                    label = "plan",
+                    backgroundColor = expenseColors,
+                    borderColor = "rgba(75, 192, 192, 1)",
+                    hoverBackgroundColor = "rgba(75, 192, 192, 0.4)",
+                    hoverBorderColor = "rgba(75, 192, 192, 1)",
+                    data = planData,
+                    barPercentage = 0.75,
+                    categoryPercentage = 0.5
+                },
+                new
+                {
+                    label = "Expense",
+                    backgroundColor =expenseColors,
+                    borderColor = "#dee2e6",
+                    hoverBackgroundColor = "#dee2e6",
+                    hoverBorderColor = "#dee2e6",
+                    data = expenseData,
+                    barPercentage = 0.75,
+                    categoryPercentage = 0.5
+                }
+            }
+            };
+
+            return Json(chartData);
+        }
+ 
+        private string[] GenerateRandomColors(int count)
+        {
+            var random = new Random();
+            var colors = new List<string>();
+
+            for (var i = 0; i < count; i++)
+            {
+                var color = string.Format("#{0:X6}", random.Next(0x1000000));
+                colors.Add(color);
+            }
+
+            return colors.ToArray();
+        }
+
     }
 }
---
+++ MyBudgetPlanner/wwwroot/js/MyBarChart.js
@@ -0,0 +1,50 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+   // var UserId = "6a60e1ff-44bc-434b-97e4-29909c05dc83";
+
+    fetch('/Home/GetChartData')
+        .then(response => response.json())
+        .then(data => {
+            new Chart(document.getElementById("MyBarChartCanvas"), {
+                type: "bar",
+                data: data,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            gridLines: {
+                                display: false
+                            },
+                            stacked: false
+                        }],
+                        xAxes: [{
+                            stacked: false,
+                            gridLines: {
+                                color: "transparent"
+                            }
+                        }]
+                    }
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('/Home/GetChartData')
+        .then(response => response.json())
+        .then(data => {
+            new Chart(document.getElementById("MyPieChartCanvas"), {
+                type: "pie",
+                data: {
+                    labels: data.labels, 
+                    datasets: [{
+                        data: data.datasets[0].data, 
+                        backgroundColor: data.datasets[0].backgroundColor,  
+                    }]
+                },
+                options: {
+                }
+            });
+        })
+        .catch(error => console.error('Error fetching data:', error));
+});
+
